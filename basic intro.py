3814dcc9c89a4a25c2e0d971a94a978419384739
Kaushal Rindhe <85434295+kaushal1796@users.noreply.github.com>
2021-08-15 04:12:50 -0700

    Add files via upload

---
+++ basic intro.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+X = [1,2,3,4,5]
+Y = [2,4,6,8,10]
+print(X)
+print(Y)
+import matplotlib.pyplot as plt
+plt.plot(X,Y)
