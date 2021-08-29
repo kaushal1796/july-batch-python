9b3a70206950c76067811a061ecc5561c9e609cd
Kaushal Rindhe <85434295+kaushal1796@users.noreply.github.com>
2021-08-29 02:50:07 -0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 15 17:32:38 2021
+
+@author: NEW
+"""
+
+list1 = [1, "hello", "world", True]
+
+list2 = list(range(10))
+
+print(list1)
+
+print(list2)
+
+print(list1[0])
+
+print(list1[2:])
+
+print(list1[-3])
+
+print(list1[2])
